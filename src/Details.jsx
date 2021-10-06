ec54caec5b28c7373158a3aaab9358b766ca0bcb
Rohan Batra <rohanbatra24@gmail.com>
2021-10-06 14:58:12 -0400

    add modal using portal

--- src/Details.jsx
+++ src/Details.jsx
@@ -2,16 +2,15 @@ import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
 import Carousel from "./Carousel";
 import ErrorBoundary from "./ErrorBoundary";
+import Modal from "./Modal";
 
 class Details extends Component {
   // a constructor always runs when an instance of the class is created
   constructor() {
     super(); //super calls the Component constructor. We always have to do this.
 
-    this.state = { loading: true, pet: null };
+    this.state = { loading: true, pet: null, modalOpen: false };
   }
-
-  state = {};
 
   async componentDidMount() {
     const res = await fetch(
@@ -36,19 +35,46 @@ class Details extends Component {
       } = this.state.pet;
 
       return (
-        <ThemeContext.Consumer>
-          <div className="details">
-            <Carousel images={images}></Carousel>
-            <div>
-              <h1>{name}</h1>
-              <h2>
-                {animal} - {breed} - {city}, {state}
-              </h2>
-              <button>Adopt {name}</button>
-              <p>{description}</p>
-            </div>
+        <div className="details">
+          <Carousel images={images}></Carousel>
+          <div>
+            <h1>{name}</h1>
+            <h2>
+              {animal} - {breed} - {city}, {state}
+            </h2>
+            <button
+              onClick={() =>
+                this.setState({ showModal: !this.state.showModal })
+              }
+            >
+              Adopt {name}
+            </button>
+            <p>{description}</p>
           </div>
-        </ThemeContext.Consumer>
+          {this.state.showModal ? (
+            <Modal>
+              <div>
+                <h1>Would you like to adopt {name}?</h1>
+                <div className="buttons">
+                  <button
+                    onClick={() =>
+                      this.setState({ showModal: !this.state.showModal })
+                    }
+                  >
+                    Yes
+                  </button>
+                  <button
+                    onClick={() =>
+                      this.setState({ showModal: !this.state.showModal })
+                    }
+                  >
+                    No
+                  </button>
+                </div>
+              </div>
+            </Modal>
+          ) : null}
+        </div>
       );
     } else {
       return <h2>Loading...</h2>;
